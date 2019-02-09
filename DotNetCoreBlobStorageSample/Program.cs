80bae9ef7d0c661ba5bc6e192ca05a1384e745d6
yTsunomoto <wasiwasimaru@gmail.com>
2019-02-10 00:17:07 +0900

    update

---
+++ DotNetCoreBlobStorageSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using DotNetCoreBlobStorageSample.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DotNetCoreBlobStorageSample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // 設定ファイルの読み込み
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            var configuration = builder.Build();
+
+            // サービスの設定
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IBlobStorageService>(factory => new BlobStorageService(configuration["ConnectionStrings:StorageConnection"]));
+            var service = serviceCollection.BuildServiceProvider().GetService<IBlobStorageService>();
+
+            // サンプル実装
+            var blobs = await service.GetBlobs("sample", "201902");
+            
+            foreach (var blob in blobs)
+            {
+                await blob.DownloadToFileAsync($".\\{blob.Name}", FileMode.OpenOrCreate);
+            }
+        }
+    }
+}
---
+++ DotNetCoreBlobStorageSample/Services/BlobStorageService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreBlobStorageSample.Services
+{
+    public interface IBlobStorageService
+    {
+        CloudBlockBlob GetBlob(string containerName, string blobName);
+        Task<List<CloudBlockBlob>> GetBlobs(string containerName, string prefix);
+
+    }
+
+    public class BlobStorageService : IBlobStorageService
+    {
+        public BlobStorageService(string connectionString)
+        {
+            SetConnectionString(connectionString);
+        }
+
+        private CloudStorageAccount cloudStorageAccount;
+
+        public void SetConnectionString(string connectionString)
+        {
+            cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
+        }
+
+        public CloudBlockBlob GetBlob(string containerName, string blobName)
+        {
+            var blobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference(containerName);
+            container.CreateIfNotExistsAsync().Wait();
+
+            return container.GetBlockBlobReference(blobName);
+        }
+
+        public async Task<List<CloudBlockBlob>> GetBlobs(string containerName, string prefix)
+        {
+            var blobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference(containerName);
+            container.CreateIfNotExistsAsync().Wait();
+            var results = new List<CloudBlockBlob>();
+
+            BlobContinuationToken continuationToken = null;
+            do
+            {
+                var listResults = await container.ListBlobsSegmentedAsync(prefix, continuationToken);
+                continuationToken = listResults.ContinuationToken;
+                results.AddRange(listResults.Results.Select(_ => (CloudBlockBlob) _));
+            } while (continuationToken != null);
+            
+            return results;
+        }
+    }
+}
