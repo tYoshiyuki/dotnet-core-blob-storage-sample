ca30660f035bdb8a40538611070ddce7e72f3e20
yTsunomoto <wasiwasimaru@gmail.com>
2019-02-10 11:32:48 +0900

    update

--- DotNetCoreBlobStorageSample/Services/BlobStorageService.cs
+++ DotNetCoreBlobStorageSample/Services/BlobStorageService.cs
@@ -6,13 +6,18 @@ using System.Threading.Tasks;
 
 namespace DotNetCoreBlobStorageSample.Services
 {
+    /// <summary>
+    /// Azure Blob Storageサービスのインターフェースです
+    /// </summary>
     public interface IBlobStorageService
     {
         CloudBlockBlob GetBlob(string containerName, string blobName);
         Task<List<CloudBlockBlob>> GetBlobs(string containerName, string prefix);
-
     }
 
+    /// <summary>
+    /// Azure Blob Storageサービスです
+    /// </summary>
     public class BlobStorageService : IBlobStorageService
     {
         public BlobStorageService(string connectionString)
@@ -20,13 +25,26 @@ namespace DotNetCoreBlobStorageSample.Services
             SetConnectionString(connectionString);
         }
 
+        /// <summary>
+        /// CloudStorageAccountです
+        /// </summary>
         private CloudStorageAccount cloudStorageAccount;
 
+        /// <summary>
+        /// ConnectionStringを設定します
+        /// </summary>
+        /// <param name="connectionString"></param>
         public void SetConnectionString(string connectionString)
         {
             cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
         }
 
+        /// <summary>
+        /// Blobを取得します
+        /// </summary>
+        /// <param name="containerName">コンテナ名</param>
+        /// <param name="blobName">Blob名</param>
+        /// <returns></returns>
         public CloudBlockBlob GetBlob(string containerName, string blobName)
         {
             var blobClient = cloudStorageAccount.CreateCloudBlobClient();
@@ -37,6 +55,12 @@ namespace DotNetCoreBlobStorageSample.Services
             return container.GetBlockBlobReference(blobName);
         }
 
+        /// <summary>
+        /// Blobのリストを取得します
+        /// </summary>
+        /// <param name="containerName">コンテナ名</param>
+        /// <param name="prefix">Blobのプレフィックス</param>
+        /// <returns></returns>
         public async Task<List<CloudBlockBlob>> GetBlobs(string containerName, string prefix)
         {
             var blobClient = cloudStorageAccount.CreateCloudBlobClient();
